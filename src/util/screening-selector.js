3959efd6e5595fa036e0d012fe5f92ab38615cad
Justin McReynolds <mcjustin@uw.edu>
2022-04-01 17:25:16 -0700

    Adds PHQ9 questionnaire to screening-selector.js ...
    More commits to come, not sure what else needs wiring up...

--- src/util/screening-selector.js
+++ src/util/screening-selector.js
@@ -2,6 +2,7 @@
 import questionnaireUsAudit from '../fhir/Questionnaire-USAUDIT.json';
 import questionnaireWhoAudit from '../fhir/Questionnaire-WHOAUDIT.json';
 import questionnaireNidaQs from '../fhir/Questionnaire-NIDAQS2USAUDIT.json';
+import questionnairePhq9 from '../fhir/Questionnaire-PHQ9.json';
 import elmJsonUsAudit from '../cql/UsAuditLogicLibrary.json';
 import elmJsonWhoAudit from '../cql/WhoAuditLogicLibrary.json';
 import elmJsonNidaQs from '../cql/NidaQsToUsAuditLogicLibrary.json';
@@ -17,6 +18,8 @@ export function getScreeningInstrument() {
     return [questionnaireWhoAudit, elmJsonWhoAudit, valueSetJsonUsAudit];
   } else if (screeningInstrument == 'nidaqs2usaudit') {
     return [questionnaireNidaQs, elmJsonNidaQs, valueSetJsonUsAudit];
+  } else if (screeningInstrument == 'phq9') {
+    return [questionnairePhq9, elmJsonPhq9, valueSetJsonPhq9];
   } else {
     throw new Error('Unsupported alcohol screening instrument has been specified');
   }
