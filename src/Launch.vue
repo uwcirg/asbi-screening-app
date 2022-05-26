9efb90ae882a995d17be4c4b9d3de5b7d0d4e107
Amy Chen <amysbubble@Amys-MacBook-Pro-2.local>
2022-05-26 15:01:04 -0700

    add error check

--- src/Launch.vue
+++ src/Launch.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="launch">
-    <div class="pa-8">
+    <div class="pa-8" v-if="!error">
       <v-progress-circular
       :value="100"
       indeterminate
@@ -8,27 +8,50 @@
       class="mr-1"></v-progress-circular>
       Loading...
     </div>
+    <div class="pa-8" v-if="error">
+      <v-alert color="error" v-if="error" dark>
+        Error launch the application.
+        <div v-html="error"></div>
+      </v-alert>
+    </div>
   </v-app>
 </template>
 
 <script>
 import FHIR from 'fhirclient';
-
 const urlParams = new URLSearchParams(window.location.search);
 let patientId = urlParams.get('patient');
-console.log("patient id from url param ", patientId)
 if (patientId == null) patientId = '123';
-FHIR.oauth2.authorize({
-    clientId: "dummy-client-id",
-    scope: "patient/Patient.read patient/Condition.read patient/Observation.read patient/Procedure.read patient/QuestionnaireResponse.read online_access openid fhirUser patient/QuestionnaireResponse.write",
-    iss: "http://localhost:3000/4_0_0",
-    launch: patientId
-});
-
+console.log("patient id from url ", patientId);
 export default {
-  name: 'Launch'
+  name: 'Launch',
+  data() {
+    return {
+      error: ""
+    }
+  },
+  mounted() {
+    let self = this;
+    fetch(`launch-context.json`)
+    .then(result => {
+      if (!result.ok) {
+        throw Error(result.statusText);
+      }
+      return result.json();
+    })
+    .then(json => {
+      if (!json.launch) {
+        json.launch = patientId;
+      }
+      console.log("launch json ", json.launch)
+      FHIR.oauth2.authorize(json).catch((e) => {
+        self.error = e;
+      });
+    })
+    .catch(e => {
+      self.error = e;
+      console.log('launch error ', e);
+    });
+  }
 }
 </script>
-
-<style>
-</style>
