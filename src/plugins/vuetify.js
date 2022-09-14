894a05317e47a5d572dad2a8ef409dbacf5f6da7
Amy Chen <achen2401@gmail.com>
2022-09-14 10:15:10 -0700

    Update src/plugins/vuetify.js
    Co-authored-by: Ivan Cvitkovic <icvitkovic@gmail.com>

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,8 +6,7 @@ import {getEnv} from '../util/util';
 
 const getAppTheme = () => {
     const projectID = getEnv("VUE_APP_PROJECT_ID");
-    if (themes[projectID] && themes[projectID].app) return themes[projectID]
-                                                      .app;
+    if (themes[projectID] && themes[projectID].app) return themes[projectID].app;
     return themes["default"].app;
 };
 
